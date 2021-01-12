db5494da2bd099d5d1bb5427a47bdb7e5c1e4a50
ananCharoensap <s6302041610073@email.kmutnb.ac.th>
2021-01-12 15:13:39 +0700

    first commit

---
+++ Addtion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// ดึง Library Input Output
+
+void main(){
+// ส่วนที่ 2 Function หลักของภาษา C คือ Fx main
+
+// ---------------- คำสั่งโปรแกรมบวกเลข --------------------- //
+    int num1, num2, total;
+    // ประกาศตัวแปร num1, num2, total
+
+    printf("Value of num1 is : ");
+    scanf("%d",&num1);
+    printf("Value of num2 is : ");
+    scanf("%d",&num2);
+
+    total = num1 + num2;
+
+    printf("Total is : %d",total);
+
+// ---------------- สิ้นสุดโปรแกรมบวกเลข -------------------- //
+
+
+}
---
+++ HelloWorld.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// ส่วนสำหรับดึง Library (1)
+// การกด ctrl + / คือการใส่คอมเมนต์แบบเร็ว short cut key
+
+// ส่วน function หลัก (2)
+int main()
+{
+    printf("Name : Anan Charoensap\n");
+    printf("NickName : Arm\n");
+    printf("Age : 18 y/o\n");
+    printf("Favorite Things : Typing\n");
+    printf("What i want to do : I want to be a teacher.\n");
+    printf("My happy : is their happy");
+    /*
+    ส่วนคำสั่งการทำงาน (3)
+        printf คือ ฟังก์ชันที่ไว้ใช้สำหรับแสดงผลทางหน้าจอ
+    */
+    //  วิธีการจัดชุดคำสั่งให้สวย ให้กด shfit + alt + f
+    //  การเปิด Terminal ให้กด ctrl + j
+}
